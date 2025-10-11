93ddfba3c14edd8fb2253a41be2d60cd6c73318b
Greg Jones <4853264+gregjones07ba@users.noreply.github.com>
2025-10-11 16:01:43 -0700

    Tutorial CDK stack.

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda, # Import the Lambda module
 )
 from constructs import Construct
 
@@ -10,10 +9,19 @@ class CdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the Lambda function resource
+        my_function = _lambda.Function(
+            self, "HelloWorldFunction",
+            runtime = _lambda.Runtime.NODEJS_20_X, # Provide any supported Node.js runtime
+            handler = "index.handler",
+            code = _lambda.Code.from_inline(
+                """
+                exports.handler = async function(event) {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify('Hello World!'),
+                    };
+                };
+                """
+            ),
+        )
